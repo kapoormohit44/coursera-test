aeb1df4aa7c37180a6b7968e96b154e61199524d
kapoormohit274 <43112120+kapoormohit274@users.noreply.github.com>
2020-07-06 10:27:17 +0530

    Update script.js

--- module4_solution/script.js
+++ module4_solution/script.js
@@ -1,24 +1,7 @@
-/*
-Solution of assignment 4:
-
-Expected output:
-
-Hello Yaakov
-Good Bye John
-Good Bye Jen
-Good Bye Jason
-Hello Paul
-Hello Frank
-Hello Larry
-Hello Paula
-Hello Laura
-Good Bye Jim
-*/
-
 (function() {
-    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+    var names = ["Mohit", "Johnson", "Jenny", "Joker", "Rohit", "Ashok", "Sunny", "Shubham", "Justin", "Riya"];
     for (var i = 0; i < names.length; i++) {
-        var firstLetter = names[i].charAt(0).toLowerCase();
+        var firstLetter = names[i].toLowerCase();
         if (firstLetter === 'j') {
             byeSpeaker.speak(names[i]);
         } else {
