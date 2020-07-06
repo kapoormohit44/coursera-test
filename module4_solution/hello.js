a8168db081b49b8053b65dab1b32031a2c29f9d7
kapoormohit274 <43112120+kapoormohit274@users.noreply.github.com>
2020-07-06 10:53:15 +0530

    Delete hello.js

--- module4_solution/hello.js
+++
@@ -1,8 +0,0 @@
-(function(window) {
-    var sayhello = new Object();
-    var word = "Hello";
-    sayhello.speak = function(name) {
-        console.log(word + " " + name);
-    };
-    window.sayhello = sayhello;
-})(window);
