f431b3f87eb299803db4426a4cb76ca706ad619f
kapoormohit274 <43112120+kapoormohit274@users.noreply.github.com>
2020-07-06 10:53:00 +0530

    Delete goodbye.js

--- module4_solution/goodbye.js
+++
@@ -1,8 +0,0 @@
-(function(window) {
-    var saybye = new Object();
-    var word = "Good Bye";
-    saybye.speak = function speak(name) {
-        console.log(word + " " + name);
-    };
-    window.saybye = saybye;
-})(window);
