9749e66f449a4291647c441e6f966cf5679f714f
kapoormohit274 <43112120+kapoormohit274@users.noreply.github.com>
2020-07-06 11:20:03 +0530

    Add files via upload

---
+++ module4_solution/script.js
@@ -0,0 +1,28 @@
+/*
+Solution of assignment 4:
+
+Expected output:
+
+Hello Yaakov
+Good Bye John
+Good Bye Jen
+Good Bye Jason
+Hello Paul
+Hello Frank
+Hello Larry
+Hello Paula
+Hello Laura
+Good Bye Jim
+*/
+
+(function() {
+    var names = ["Mohit", "Johnson", "Jenny", "Joker", "Rohit", "Ashok", "Sunny", "Shubham", "Justin", "Riya"];
+    for (var i = 0; i < names.length; i++) {
+        var firstLetter = names[i].charAt(0).toLowerCase();
+        if (firstLetter === 'j') {
+            byeSpeaker.speak(names[i]);
+        } else {
+            helloSpeaker.speak(names[i]);
+        }
+    }
+})();
